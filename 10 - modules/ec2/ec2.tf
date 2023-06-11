0ccd64a9e754f2532a6adcba767cdb9acef27a35
Julius <use.julius.gutierrez@gmail.com>
2023-06-11 23:22:46 +0800

    learn to pass output from module

--- 10 - modules/ec2/ec2.tf
+++ 10 - modules/ec2/ec2.tf
@@ -3,10 +3,14 @@ variable "ec2name" {
 }
 
 
-resource "aws_instance" "dbserver" {
+resource "aws_instance" "ec2" {
   ami = "ami-032598fcc7e9d1c7a"
   instance_type = "t2.micro"
   tags = {
         Name = var.ec2name
   }
+}
+
+output "instance_id" {
+  value = aws_instance.ec2.id
 }
